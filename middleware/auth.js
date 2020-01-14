f3eb037d590a49e7669bef1f3cf6b73d2cc50d31
an4ger <an4ger@gmail.com>
2020-01-14 13:06:45 +0200

    api security

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,28 +7,30 @@ const User = require('../models/User')
 exports.protect = asyncHandler(async (req, res, next) => {
     let token
 
+    // Set token from header
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1]
     }
+
+    // Set token from cookie
     // else if (req.cookie.token) {
     //     token = req.cookie.token
     // }
 
     // Make sure token exists
     if (!token) {
-        return next(new ErrorResponse('Not authorize to access this route', 401))
+        return next(new ErrorResponse('Not authorized to access this route', 401))
     }
 
     try {
         // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded)
 
         req.user = await User.findById(decoded.id)
 
         next()
     } catch (err) {
-        return next(new ErrorResponse('Not authorize to access this route', 401))
+        return next(new ErrorResponse('Not authorized to access this route', 401))
     }
 })
 
